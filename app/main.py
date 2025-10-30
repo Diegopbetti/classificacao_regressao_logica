67fd348df703ddb23d4d5c108ad01b1de5d8991a
Diego Pereira Betti <diegopbetti.gmail.com>
2025-10-30 12:27:52 -0300

    Cria imagem docker

--- app/main.py
+++ app/main.py
@@ -23,11 +23,11 @@ modelo_qualidade = joblib.load('./modelo_qualidade_frutas.pkl')
 @app.post('/classify')
 def predict(data: request_body):
     # Preparar features
-    input_features = [[data.Size, data.Juiciness, data.Ripeness, data.Acidity]]
+    input_features = [[data.Size, data.Weight, data.Sweetness, data.Crunchiness, data.Juiciness, data.Ripeness, data.Acidity]]
 
     # Classificar Fruta
     y_pred = modelo_qualidade.predict(input_features)[0].astype(int)
-    y_prob = modelo_qualidade.predict_proba(input_features)[0].atype(float)
+    y_prob = modelo_qualidade.predict_proba(input_features)[0].astype(float)
 
     resposta = 'Boa' if y_pred == 1 else 'Ruim'
     probabilidade = y_prob[y_pred]
